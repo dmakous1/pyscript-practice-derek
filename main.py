84c1ca32039cce8c5596df1307da467ca0fbba52
dmakous1 <100861805+dmakous1@users.noreply.github.com>
2024-02-24 17:17:52 -0500

    removed example garbage (and swapped xy...)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
 import numpy as np
 
 from pyweb import pydom
@@ -21,7 +20,7 @@ def update_graph(event):
 	y_model = 2*x_model*x_model
 	fig1, ax1 = plt.subplots(1,dpi=150,figsize=(5,3))
 	fig1, ax1 = plt.subplots()
-	ax1.scatter(yfloat,xfloat)
+	ax1.scatter(xfloat,yfloat)
 	ax1.set_title("Graph of Data",fontsize=11)
 	ax1.set_xlabel("x variable",fontsize=10)
 	ax1.set_ylabel("y variable",fontsize=10)
@@ -31,33 +30,3 @@ def update_graph(event):
 	ax1.margins(y=0)
 	ax1.grid()
 	display(fig1, target='graph', append=False)
-
-# First create the x and y coordinates of the points.
-n_angles = 36
-n_radii = 8
-min_radius = 0.25
-radii = np.linspace(min_radius, 0.95, n_radii)
-
-angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
-angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
-angles[:, 1::2] += np.pi / n_angles
-
-x = (radii * np.cos(angles)).flatten()
-y = (radii * np.sin(angles)).flatten()
-z = (np.cos(radii) * np.cos(3 * angles)).flatten()
-
-# Create the Triangulation; no triangles so Delaunay triangulation created.
-triang = tri.Triangulation(x, y)
-
-# Mask off unwanted triangles.
-triang.set_mask(np.hypot(x[triang.triangles].mean(axis=1),
-                            y[triang.triangles].mean(axis=1))
-                < min_radius)
-
-fig1, ax1 = plt.subplots()
-ax1.set_aspect('equal')
-tpc = ax1.tripcolor(triang, z, shading='flat')
-fig1.colorbar(tpc)
-ax1.set_title('tripcolor of Delaunay triangulation, flat shading')
-
-display(fig1, target="mpl")
